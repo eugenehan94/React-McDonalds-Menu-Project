db6009fc6c3a0232b309fcfd624cc4fbf03283b1
eugenehan94 <eugene.han94@gmail.com>
2021-06-10 17:02:25 -0400

    made some updates

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,35 @@
-import MenuOptions from "./MenuOptions";
+import "../css/Categories.css";
 
-const Categories = ({ categories, filtering }) => {
+const Categories = ({
+  categories,
+  filtering,
+  fullMenu,
+  selected,
+  menuOptions,
+}) => {
   return (
     <div>
-      {categories.map((item, id) => {
-        return (
-          <div key={id}>
-            <button type="button" onClick={() => filtering(item)}>
-              {item}
-            </button>
-          </div>
-        );
-      })}
+      <div className="button-reset-container">
+        <button type="button" onClick={() => fullMenu()}>
+          View Temporary Limited Menu
+        </button>
+      </div>
+      <div className="button-container">
+        {categories.map((item, id) => {
+          return (
+            <div key={id} className={item === selected ? "active_choice" : ""}>
+              <button
+                type="button"
+                className={item === selected ? "active_bold" : ""}
+                onClick={() => filtering(item)}
+              >
+                menuOptions.
+                {item}
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
